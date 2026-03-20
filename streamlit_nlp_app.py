ca5968b7bef08f77e6d1dca6d1becb5b7465b62e
SK <venkat.mce38@gmail.com>
2026-03-20 13:20:41 +0530

    modifiedCode

--- streamlit_nlp_app.py
+++ streamlit_nlp_app.py
@@ -1891,7 +1891,7 @@ def main():
     """Main Streamlit application"""
     st.set_page_config(
         page_title="Intelli-CXMiner",
-        page_icon="🧐",
+        page_icon="🔍",
         layout="wide",
         initial_sidebar_state="collapsed"
     )
